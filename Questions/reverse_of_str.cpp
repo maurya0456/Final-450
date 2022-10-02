f914070516e480d7c18d97ae1bd43f3dbc3ae103
Aman <amanmaurya4527@gmail.com>
2022-10-02 07:35:41 +0530

    commit on 2 october

--- Questions/reverse_of_str.cpp
+++ Questions/reverse_of_str.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char s[50]="aman";
+    char s[5]="aman";
+    cout<<sizeof(s)<<endl;
+    char arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i]=s[4-i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<arr[i]<<"";
+    }
+    
     
 
 return 0;
