f8c98e58135c9528d8cf1cd588878bce8efafa5c
Aman <amanmaurya4527@gmail.com>
2022-10-02 09:00:47 +0530

    direct commit ,withut staging area add

--- Questions/first_Program.cpp
+++ Questions/first_Program.cpp
@@ -5,3 +5,14 @@ int main()
 {
   cout << "Welcome .";
 }
+
+/*git diff aapko batata hai 
+kya kya difference the ,given file 
+ko dusri file se compare karta hai 
+working tree ko compare karata 
+hai staging area se .*/
+
+/*when we use 
+get diff --staged 
+staging area ko last 
+commit se compare karta hai*/
