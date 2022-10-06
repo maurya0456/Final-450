549086d9fce7b1613fd21e8d9604f136f273674c
Aman <amanmaurya4527@gmail.com>
2022-10-06 08:22:36 +0530

    added car feature

---
+++ car.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        while(n!=0){
+            string s;
+            cin>>s;
+            cout<<s<<endl;
+            n--;
+        }
+
+
+    }
+    return 0;
+}
